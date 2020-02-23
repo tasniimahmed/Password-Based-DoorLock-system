8274caf2976175d6634c6bc8067ed3c1bd863e84
AmrElsersy <amrelsersay@gmail.com>
2020-02-23 21:34:08 +0200

    UART_v1

--- UART/uart.h
+++ UART/uart.h
@@ -1,72 +1,70 @@
-#ifndef UART_H
-#define UART_H
+#include "D:/std_types.h"
+#include "C:/Keil_v5/EE319KwareSpring2020/inc/tm4c123gh6pm.h"
 
-#include <stdio.h>
-#include "A:/Some Requirements/Keil/tm4c123gh6pm.h"
-#include "A:/Password-Based-DoorLock-system/UART/std_types.h"
 
+
+
+/************************** Enums & Structs ***************************/ 
 typedef enum
 {
-PARITY_ENABLE=1,
-PARITY_DISABLE=0
+	PARITY_ENABLE=1, 
+	PARITY_DISABLE=0 
 }UART_ParityEnable;
 
-typedef enum
+typedef enum 
 {
-FIFO_ENABLE =1 ,
-FIFO_DISABLE=0
+	FIFO_ENABLE =1 , 
+	FIFO_DISABLE=0
 }UART_FIFO;
 
-typedef enum
+typedef enum 
 {
-UART_WORDLENGTH_5B=0x0,
-UART_WORDLENGTH_6B=0x1,
-UART_WORDLENGTH_7B=0x2,
-UART_WORDLENGTH_8B=0x3
+	UART_WORDLENGTH_5B=0x0, 
+	UART_WORDLENGTH_6B=0x1,
+	UART_WORDLENGTH_7B=0x2,
+	UART_WORDLENGTH_8B=0x3
 }UART_WordLength;
 
-typedef enum
+typedef enum 
 {
-UART_STOPBITS_0=0 ,
-UART_STOPBITS_1=1
+	UART_STOPBITS_1=0 , 
+	UART_STOPBITS_2=1
 }UART_StopBits;
 
 typedef enum{
-   ODD_PARITY = 1,
-   EVEN_PARITY =0
+    ODD_PARITY = 1,
+    EVEN_PARITY =0
 }UART_ParitySelect;
 
 typedef enum{
-   HIGH_SPEED_ENABLE = 1,
-   HIGH_SPEED_DISABLE = 0
+    HIGH_SPEED_ENABLE = 1,
+    HIGH_SPEED_DISABLE = 0
 }UART_HighSpeed;
 
 typedef enum{
-   TRANSMIT_ENABLE = 1,
-   TRANSMIT_DISABLE = 0
+    TRANSMIT_ENABLE = 1,
+    TRANSMIT_DISABLE = 0
 }UART_TransmitEnable;
 
 typedef enum{
-   RECEIVE_ENABLE = 1,
-   RECEIVE_DISABLE = 0
+    RECEIVE_ENABLE = 1,
+    RECEIVE_DISABLE = 0
 }UART_ReceiveEnable;
 
 typedef struct
 {
-uint32_t BaudRate;
-UART_WordLength WordLength;
-UART_StopBits StopBits;
-UART_FIFO FIFO;
-UART_ParityEnable ParityEnable;
-UART_ParitySelect ParitySelect;
-UART_HighSpeed HighSpeed ;
-UART_ReceiveEnable ReceiveEnable;
-UART_TransmitEnable TransmitEnable ;
-
+	uint32_t BaudRate; 	
+	UART_WordLength WordLength; 
+	UART_StopBits StopBits;
+	UART_FIFO FIFO; 
+	UART_ParityEnable ParityEnable;
+	UART_ParitySelect ParitySelect;
+	UART_HighSpeed HighSpeed ; 
+	UART_ReceiveEnable ReceiveEnable; 
+	UART_TransmitEnable TransmitEnable ; 
+	
 }UART0_HandleTypedef;
 
 
-		/***************** FUNCTION APIs *****************/
-void UART_Init(UART0_HandleTypedef * const);
-#endif 
-
+/*****************************FUNCTION DECLARATIONS*************************************/ 
+void UART_init(UART0_HandleTypedef *const uartConfig);
